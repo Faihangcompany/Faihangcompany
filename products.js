5aa82b25442563b76394f556df0e5f5e35447249
Faihangcompany <faihangcompany@gmail.com>
2026-02-05 18:02:57 +0800

    Create products.js

---
+++ products.js
@@ -0,0 +1,104 @@
+// products.js - 你的真實產品資料
+const products = [
+    {
+        category: "裝飾櫃",
+        code: "TR0020",
+        size: "500*395*1020",
+        color: "ORI胡桃、親橡、橡木",
+        originalPrice: 1160,
+        salePrice: 986,
+        note: "鋁腳高度90mm，玻璃厚度8mm，鋁腳和鋁框顏色為香檳金，拉手顏色為銀色。",
+        mattressSize: "/"
+    },
+    {
+        category: "餐邊櫃",
+        code: "TR0025-40",
+        size: "1016*395*845",
+        color: "ORI胡桃、親橡、橡木",
+        originalPrice: 1850,
+        salePrice: 1573,
+        note: "鋁腳高度90mm，玻璃厚度8mm，鋁腳和鋁框顏色為香檳金，拉手顏色為銀色。",
+        mattressSize: "/"
+    },
+    {
+        category: "餐邊櫃",
+        code: "TR0025-48",
+        size: "1200*395*845",
+        color: "ORI胡桃、親橡、橡木",
+        originalPrice: 2280,
+        salePrice: 1938,
+        note: "鋁腳高度90mm，玻璃厚度8mm，鋁腳和鋁框顏色為香檳金，拉手顏色為銀色。",
+        mattressSize: "/"
+    },
+    {
+        category: "餐邊櫃",
+        code: "TR0025-60",
+        size: "1525*395*845",
+        color: "ORI胡桃、親橡、橡木",
+        originalPrice: 2670,
+        salePrice: 2270,
+        note: "鋁腳高度90mm，玻璃厚度8mm，鋁腳和鋁框顏色為香檳金，拉手顏色為銀色。",
+        mattressSize: "/"
+    },
+    {
+        category: "餐邊櫃",
+        code: "TR0045H-48",
+        size: "1194*400*2160",
+        color: "白木紋",
+        originalPrice: 3850,
+        salePrice: 3273,
+        note: "鋁腳高度90mm，顏色為香檳金。",
+        mattressSize: "/"
+    },
+    {
+        category: "餐邊櫃",
+        code: "TR010",
+        size: "428*400*980",
+        color: "白木紋",
+        originalPrice: 860,
+        salePrice: 731,
+        note: "鋁腳高度90mm，顏色為香檳金，拉手顏色為銀色。",
+        mattressSize: "/"
+    },
+    {
+        category: "餐邊櫃",
+        code: "TR020",
+        size: "855*400*980",
+        color: "白木紋",
+        originalPrice: 1500,
+        salePrice: 1275,
+        note: "鋁腳高度90mm，顏色為香檳金，拉手顏色為銀色。",
+        mattressSize: "/"
+    },
+    {
+        category: "地櫃",
+        code: "TR083-48",
+        size: "1200*395*595",
+        color: "白木紋、橡木",
+        originalPrice: 1650,
+        salePrice: 1403,
+        note: "鋁腳高度90mm，鋁腳、鋁框和烤漆拉手顏色為香檳金。",
+        mattressSize: "/"
+    },
+    {
+        category: "地櫃",
+        code: "TR083-60",
+        size: "1525*395*595",
+        color: "ORI胡桃、親橡、白木紋、橡木",
+        originalPrice: 2280,
+        salePrice: 1938,
+        note: "鋁腳高度90mm，鋁腳、鋁框和烤漆拉手顏色為香檳金。",
+        mattressSize: "/"
+    },
+    {
+        category: "地櫃連上座",
+        code: "TR083S-60",
+        size: "1525*316*1565",
+        color: "白木紋",
+        originalPrice: 2650,
+        salePrice: 2253,
+        note: "鋁框顏色為香檳金，玻璃層板厚度為8mm。",
+        mattressSize: "/"
+    }
+    // TODO: 把你的其他90個產品加到這裡，複製上面格式
+];
